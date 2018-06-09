c4ffa9bc5f27812bf30a01474d4d1889a1488968
thedarkprincedc <brettmosley@hotmail.com>
2018-06-09 16:46:17 -0400

    added code

--- components/app.constants.js
+++ components/app.constants.js
@@ -1,17 +1,17 @@
 (function(window) {
     'use strict';
     window.__env = window.__env || {};
-    window.__env.apiUrl = '';
+    var apiUrl = (window.__env.apiUrl) ? window.__env.apiUrl : '';
     angular.module('app.constants', [])
         .constant("URIS", {
-            GAME_SEARCH_URL:  window.__env.apiUrl + "/api/games",
-            GAME_SYSTEM_URL:  window.__env.apiUrl + "/api/systems",
-            GAME_YEARS_URL:  window.__env.apiUrl + "/api/years",
-            GAME_DECADES_URL:  window.__env.apiUrl + "/api/decades",
-            AUTH_LOGIN_URL:   window.__env.apiUrl + "/auth/login",
-            AUTH_LOGOUT_URL:  window.__env.apiUrl + "/auth/logout",
-            GAMESDB_SEARCH:  window.__env.apiUrl +"/api/gamesdb/search",
-            YOUTUBE_SEARCH:  window.__env.apiUrl+ "/api/youtube/search",
+            GAME_SEARCH_URL:  apiUrl + "/api/games",
+            GAME_SYSTEM_URL:  apiUrl + "/api/systems",
+            GAME_YEARS_URL:  apiUrl + "/api/years",
+            GAME_DECADES_URL:  apiUrl + "/api/decades",
+            AUTH_LOGIN_URL:  apiUrl + "/auth/login",
+            AUTH_LOGOUT_URL:  apiUrl + "/auth/logout",
+            GAMESDB_SEARCH:  apiUrl + "/api/gamesdb/search",
+            YOUTUBE_SEARCH:  apiUrl + "/api/youtube/search",
             YOUTUBE_EMBED_URL: "https://www.youtube.com/embed/",
             AMAZON_S3_BUCKET_URL: "https://s3-us-west-2.amazonaws.com/media.thedarkprincedc.com/images/"     
         });
